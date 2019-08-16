5f39b79b2eb2155be7e45e01904986227fce66ef
mossoii84 <mossomkh@gmail.com>
2019-08-16 22:32:12 +0200

    С table-Material+@manytomany+Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent} from './employee/employee.component';
+import { ServiceEmployee } from './employee/serviceemployee';
+import { ProjectComponent } from './project/project.component';
+import {ServiceProject} from './project/serviceproject';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {
+  MatTableModule, MatSelectModule, MatOptionModule, MatFormFieldModule, MatButtonModule, MatInputModule,
+  MatIconModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: '', component: EmployeeComponent },
+  { path: 'project', component: ProjectComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ProjectComponent,
+
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,ReactiveFormsModule,
+    MatTableModule,
+    MatSelectModule, MatOptionModule, MatFormFieldModule, MatButtonModule,MatInputModule,MatIconModule,
+    BrowserAnimationsModule,BrowserModule,
+    RouterModule.forRoot(appRoutes)
+    ],
+  providers: [ServiceEmployee,ServiceProject],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+exports: [RouterModule]
